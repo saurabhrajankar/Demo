e31dba7045ebec419136bbf1d4a0e1676addfaaa
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2023-05-24 07:58:23 +0530

    Add files via upload

---
+++ Demo1/Demo1/Controllers/DemoController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System;
+using Microsoft.Data.SqlClient;
+
+namespace Demo1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : Controller
+    {
+        private readonly IConfiguration iconfiguration;
+
+        public DemoController(IConfiguration iconfiguration)
+        {
+            this.iconfiguration = iconfiguration;
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(Model model)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(iconfiguration.GetConnectionString("newdata")))
+                {
+                    SqlCommand cmd = new SqlCommand("adda", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@name", model.name);
+                    cmd.Parameters.AddWithValue("@city", model.city);
+                    cmd.Parameters.AddWithValue("@mobileno", model.mobileno);
+                    con.Open();
+                    int result = cmd.ExecuteNonQuery();         
+                    con.Close();
+
+                    if (result > 0)
+                    {
+                        return Ok();
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Demo1/Demo1/Model.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo1
+{
+    public class Model
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string city { get; set; }
+        public string mobileno { get; set; }
+    }
+}
